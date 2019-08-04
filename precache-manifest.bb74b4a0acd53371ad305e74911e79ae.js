7483af7f634060fec2c49fe0458b4a1f81b34f1d
Serge Krstic <srdjankrstic@gmail.com>
2019-08-04 12:50:50 +0800

    Updates

---
+++ precache-manifest.bb74b4a0acd53371ad305e74911e79ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b40d61286a477acdb84bbb558a11dff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9d2f6faa8096bf28c70",
+    "url": "/monsters-rolodex/static/css/main.1a744f9b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e9d2f6faa8096bf28c70",
+    "url": "/monsters-rolodex/static/js/main.0d04fbab.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
